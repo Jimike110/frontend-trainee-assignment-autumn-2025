bdc56a0c76b8d800b017c15232aed2d95e963e0e
Jimike110 <oladoyemike@gmail.com>
2025-11-13 12:21:08 +0300

    fix: fixed cors error by adding cors middleware

--- tech-int3-server/server.js
+++ tech-int3-server/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
+const allowedOrigins = ['http://localhost:5173'];
+
+const options = {
+  origin: allowedOrigins
+}
+
+app.use(cors(options));
 app.use('/api/v1', require('./src/routes/v1'));
 
 app.get('/', (req, res) => {
